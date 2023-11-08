e8e6529239479f90dcacda8ba5db0fd13e728d81
Bullets-jyr <jyr@bullets.co.kr>
2023-11-08 12:02:23 +0900

    Initial Commit

---
+++ WarmUp/WarmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmUpApp.swift
+//  WarmUp
+//
+//  Created by xinapse on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WarmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
