8383c134d3a13bf1270c60befb05a344b0f9188f
Bullets-jyr <jyr@bullets.co.kr>
2023-11-08 12:16:02 +0900

    01. 코딩 없이 화면 그리기

--- WarmUp/ContentView.swift
+++ WarmUp/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+                .imageScale(/*@START_MENU_TOKEN@*/.large/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.green)
+                .padding(.top)
         }
         .padding()
     }
