7211b20f15145a2b985e6f4d35fb789d3699adc4
Bullets-jyr <jyr@bullets.co.kr>
2023-11-09 12:10:24 +0900

    02. 간단한 화면에 필요한 요소들 손대보기

---
+++ WarmUp/SimpleView.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleView.swift
+//  WarmUp
+//
+//  Created by xinapse on 11/9/23.
+//
+
+import SwiftUI
+
+struct SimpleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimpleView()
+}
