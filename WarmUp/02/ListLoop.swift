e7da56d119381e12165ffc27fe859592f4881938
Bullets-jyr <jyr@bullets.co.kr>
2023-11-13 12:06:11 +0900

    01. 리스트와 반복

---
+++ WarmUp/02/ListLoop.swift
@@ -0,0 +1,18 @@
+//
+//  ListLoop.swift
+//  WarmUp
+//
+//  Created by xinapse on 11/13/23.
+//
+
+import SwiftUI
+
+struct ListLoop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListLoop()
+}
