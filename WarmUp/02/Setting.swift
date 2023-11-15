700d282a1788192ef86fdc4771c8e93d4907f07b
Bullets-jyr <jyr@bullets.co.kr>
2023-11-15 12:03:16 +0900

    04. 간단한 리스트를 사용한 앱

---
+++ WarmUp/02/Setting.swift
@@ -0,0 +1,18 @@
+//
+//  Setting.swift
+//  WarmUp
+//
+//  Created by xinapse on 11/14/23.
+//
+
+import SwiftUI
+
+struct Setting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Setting()
+}
