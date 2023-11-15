28274c48a7e27664fce5c449de7472095e090703
Bullets-jyr <jyr@bullets.co.kr>
2023-11-15 12:26:00 +0900

    01. 여러 화면을 이동하기

---
+++ WarmUp/03/Detail.swift
@@ -0,0 +1,18 @@
+//
+//  Detail.swift
+//  WarmUp
+//
+//  Created by xinapse on 11/15/23.
+//
+
+import SwiftUI
+
+struct Detail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Detail()
+}
---
+++ WarmUp/03/Modal.swift
@@ -0,0 +1,18 @@
+//
+//  Modal.swift
+//  WarmUp
+//
+//  Created by xinapse on 11/15/23.
+//
+
+import SwiftUI
+
+struct Modal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Modal()
+}
