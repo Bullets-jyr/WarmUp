135e2badbbd9a4293e37327f05ff2d1a0e8061fd
Bullets-jyr <jyr@bullets.co.kr>
2023-11-20 12:33:23 +0900

    02. 네비게이션 써보기

---
+++ WarmUp/03/Navigation.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation.swift
+//  WarmUp
+//
+//  Created by xinapse on 11/20/23.
+//
+
+import SwiftUI
+
+struct Navigation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation()
+}
