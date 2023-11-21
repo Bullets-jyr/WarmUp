cda63288168503f6c01b3757d4f7134012fb44de
Bullets-jyr <jyr@bullets.co.kr>
2023-11-21 12:03:32 +0900

    03. 탭뷰로 그려보기

---
+++ WarmUp/03/MyTab.swift
@@ -0,0 +1,18 @@
+//
+//  MyTab.swift
+//  WarmUp
+//
+//  Created by xinapse on 11/21/23.
+//
+
+import SwiftUI
+
+struct MyTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyTab()
+}
---
+++ WarmUp/03/TabDetail.swift
@@ -0,0 +1,18 @@
+//
+//  TabDetail.swift
+//  WarmUp
+//
+//  Created by xinapse on 11/21/23.
+//
+
+import SwiftUI
+
+struct TabDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabDetail()
+}
